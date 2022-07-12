fd6ce81639db3e9d73d388950b371ea14a4b0e1f
Rodrigoxalmeida <rodrigox.almeida@yahoo.com.br>
2022-07-11 21:56:12 -0300

    Instancia base de dados

--- src/main/java/br/com/bookstore/BookstoreApplication.java
+++ src/main/java/br/com/bookstore/BookstoreApplication.java
@@ -1,13 +1,40 @@
 package br.com.bookstore;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.bookstore.domain.Categoria;
+import br.com.bookstore.domain.Livro;
+import br.com.bookstore.repositories.CategoriaRepository;
+import br.com.bookstore.repositories.LivroRepository;
+
 @SpringBootApplication
-public class BookstoreApplication {
+public class BookstoreApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	@Autowired
+	private LivroRepository livroRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null,"Informatica", "Livro de informatica");
+		
+		Livro l1 = new Livro(null, "Clean Code", "Robert Martin", "Lorem ipsum", cat1);
+		
+		cat1.getLivros().addAll(Arrays.asList(l1));
+		
+		this.categoriaRepository.saveAll(Arrays.asList(cat1));
+		this.livroRepository.saveAll(Arrays.asList(l1));
+		
 	}
 
 }
