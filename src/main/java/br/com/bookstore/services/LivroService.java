21ef051a231400b0dced9df71dd0a5d6fdab44c7
Rodrigoxalmeida <rodrigox.almeida@yahoo.com.br>
2022-07-21 22:33:17 -0300

    Update, delete e create  para Livros

--- src/main/java/br/com/bookstore/services/LivroService.java
+++ src/main/java/br/com/bookstore/services/LivroService.java
@@ -4,9 +4,11 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import br.com.bookstore.domain.Livro;
+import br.com.bookstore.dtos.LivroDTO;
 import br.com.bookstore.repositories.LivroRepository;
 import br.com.bookstore.services.exceptions.ObjectNotFoundException;
 
@@ -23,6 +25,29 @@ public class LivroService {
 	
 	public List<Livro> findAll(){
 		return repository.findAll();
+	}
+	
+	public Livro create(Livro obj) {
+		obj.setId(null);
+		return repository.save(obj);
+	}
+	
+	public Livro update(Integer id, LivroDTO objDTO) {
+		Livro obj = findById(id);
+		obj.setTitulo(objDTO.getTitulo());
+		obj.setNome_autor(objDTO.getNome_autor());	
+		obj.setTexto(objDTO.getTexto());			
+		return repository.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		findById(id);
+		try {
+			repository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new br.com.bookstore.services.exceptions.DataIntegrityViolationException(
+					"Erro ao deletar livro!");
+		}
 	}
 
 }
